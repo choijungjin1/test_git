6c0f3dc75a66e2e1b5c30c5741601f13c66d25ad
choijungjin1 <cjj1002@naver.com>
2019-07-16 18:03:47 +0900

    grid commit

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const app = express()
 const port = 3000
-
 //app.get('/', (req, res) => res.send('Hello World!'))
 
 //app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 //app.use(express.static('views'))
 
+app.use(express.static('public'));
 const path = require('path');
 const router = express.Router();
 router.get('/', function (req, res) {
@@ -21,6 +21,37 @@ router.get('/bootstrap_a', function (req, res) {
 
 router.get('/bootstrap_b', function (req, res) {
     res.sendFile(path.join(__dirname + '/views/bootstrap_b.html'));
+//__dirname : It will resolve to your project folder.
+});
+
+router.get('/a_utilities', function (req, res) {
+    res.sendFile(path.join(__dirname + '/views/a_utilities.html'));
+//__dirname : It will resolve to your project folder.
+});
+
+router.get('/a_grid', function (req, res) {
+    res.sendFile(path.join(__dirname + '/views/a_grid.html'));
+//__dirname : It will resolve to your project folder.
+});
+
+router.get('/a_grid02', function (req, res) {
+    res.sendFile(path.join(__dirname + '/views/a_grid02.html'));
+//__dirname : It will resolve to your project folder.
+});
+router.get('/a_grid03', function (req, res) {
+    res.sendFile(path.join(__dirname + '/views/a_grid03.html'));
+//__dirname : It will resolve to your project folder.
+});
+router.get('/a_grid04', function (req, res) {
+    res.sendFile(path.join(__dirname + '/views/a_grid04.html'));
+//__dirname : It will resolve to your project folder.
+});
+router.get('/a_grid05', function (req, res) {
+    res.sendFile(path.join(__dirname + '/views/a_grid05.html'));
+//__dirname : It will resolve to your project folder.
+});
+router.get('/a_grid06', function (req, res) {
+    res.sendFile(path.join(__dirname + '/views/a_grid06.html'));
 //__dirname : It will resolve to your project folder.
 });
 app.use('/', router);
