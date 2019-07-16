3ae5bba2e5a69398a52b481a54de44d5a995ae71
byhero <pjm93love@naver.com>
2019-07-16 12:44:31 +0900

    initial commits

--- app.js
+++ app.js
@@ -13,8 +13,13 @@ router.get('/', function (req, res) {
 res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-router.get('/external', function (req, res) {
-    res.sendFile(path.join(__dirname + '/views/external01.html'));
+router.get('/bootstrap_a', function (req, res) {
+    res.sendFile(path.join(__dirname + '/views/bootstrap_a.html'));
+//__dirname : It will resolve to your project folder.
+});
+
+router.get('/bootstrap_b', function (req, res) {
+    res.sendFile(path.join(__dirname + '/views/bootstrap_b.html'));
 //__dirname : It will resolve to your project folder.
 });
 app.use('/', router);
